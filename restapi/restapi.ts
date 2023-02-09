2703fda1a517bcd7509782f61f6b2408f256f6d6
Agoen <agoen@purdue.edu>
2023-02-09 13:08:27 -0500

    updated URL_FILE, added run batch script outside of restapi, gave compatibility scores

--- restapi/restapi.ts
+++ restapi/restapi.ts
@@ -1,5 +1,9 @@
 #!/usr/bin/env ts-node
 import { readFileSync } from 'fs';
+import { Octokit } from "octokit";
+const octokit = new Octokit({ 
+    auth: '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',
+});
 
 // This function grabs the license information for a github repository
 async function FetchGithubRepo(owner:string, repo:string) {
@@ -9,8 +13,10 @@ async function FetchGithubRepo(owner:string, repo:string) {
     const url = "https://github.com/" + owner + "/" + repo
     try {
         console.log(data['license']['key'], url)
+        console.log(CheckCompatibility(data['license']['key']))
     } catch(error) {
         console.log("N/A", url)
+        console.log(CheckCompatibility('N/A'))
     }
 }
 
@@ -22,8 +28,10 @@ async function FetchNPMRepo(name:string) {
     const url = "https://www.npmjs.com/package/" + name
     try {
         console.log(data['license'], url);
+        console.log(CheckCompatibility(data['license']))
     } catch(error) {
         console.log("N/A", url)
+        console.log(CheckCompatibility('N/A'))
     }
 }
 
@@ -46,8 +54,18 @@ function RegexLink(textfile:string) {
 
 }
 
-async function CheckCompatibility() {
-
+function CheckCompatibility(license:string) {
+    // These lists of compatible and incompatible licenses are based on documents found online unger the GPL licensing information website, will be linked in readme
+    // If its listed as other, that means that there is a license, but not explicitly stated within the repository and is under a readme.
+    // We were not able to regex readme, so we are assuming that lgpl is compatible as it is more common than not, compatible with licenses
+    let incompatible: Array<string> = ['afl-3.0', 'cc', 'cc0-1.0', 'cc-by-4.0', 'epl-1.0', 'epl-2.0', 'agpl-3.0', 'postgresql', 'N/A']
+    let compatible: Array<string> = ['artistic-2.0', 'bsl-1.0', 'bsd-2-clause', 'bsd-3-clause', 'bsd-3-clause-clear', 'mit', 'MIT', 'wtfpl', 'gpl', 'gpl-2.0', 'gpl-3.0', 'lgpl', 'lgpl-2.1', 'lgpl-3.0', 'isc', 'lppl-1.3c', 'ms-pl', 'mpl-2.0', 'osl-3.0', 'ofl-1.1', 'unlicense', 'zlib', 'ncsa', 'other', 'apache-2.0']
+    // These lists will now compared with the license passed in the parameter to see it is compatible
+    let score = 0.0
+    if (compatible.includes(license)) {
+        score = 1.0
+    }
+    return score
 }
 
 RegexLink(process.argv.slice(2)[0])
