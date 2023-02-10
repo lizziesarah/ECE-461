d88c05325eb64996bea19928c784fa58dca0bfd0
lizziesarah <102872411+lizziesarah@users.noreply.github.com>
2023-02-10 13:34:53 -0800

    compiled graphql queries

--- compiledqueries.py
+++ compiledqueries.py
@@ -24,7 +24,7 @@ def readFile(urlfile):
         return owners_and_names
 
 
-def getBusFactorScore(username, token, owner, name, outfile):
+def getBusFactorScore(username, token, owner, name):
     #username = "lizziesarah"
     #token = "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"
     header = {'Authorization': 'Bearer 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'}
@@ -36,6 +36,7 @@ def getBusFactorScore(username, token, owner, name, outfile):
 
     # req=requests.get(url='https://api.github.com/graphql', auth=(username,token)) headers=header
     req = requests.post(url='https://api.github.com/graphql', json={'query': query1}, auth=(username, token))
+
     result = req.json()
 
     numContributors = result['data']['repository']['mentionableUsers']['totalCount']
@@ -45,12 +46,10 @@ def getBusFactorScore(username, token, owner, name, outfile):
         score = numContributors / 5
     #print(f"Number of contributors: {numContributors}")
     #print(f"Bus Factor: {score}")
-    outfile_pointer = open(outfile, "a")
-    outfile_pointer.write(f'\n\t'+'{'+f'\n\t\tBus Factor: {score}\n'+'\t}')
-    outfile_pointer.close()
+
     return score
 
-def getCorrectnessScore(username, token, owner, name, outfile) :
+def getCorrectnessScore(username, token, owner, name) :
     #username = "lizziesarah"
     #token = "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"
     header = {'Authorization': 'Bearer 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'}
@@ -69,17 +68,11 @@ def getCorrectnessScore(username, token, owner, name, outfile) :
     score = 0
     if number_of_stars > 5:
         score=1
-        outfile_pointer = open(outfile, "a")
-        outfile_pointer.write(f'\n\t'+'{'+f'\n\t\tCorrectness: {score}\n'+'\t}')
-        outfile_pointer.close()
         return 1
     else:
-        outfile_pointer = open(outfile, "a")
-        outfile_pointer.write(f'\n\t'+'{'+f'\n\t\tCorrectness: {score}\n'+'\t}')
-        outfile_pointer.close()
         return 0
 
-def getResponsiveMaintainersScore(username, token, owner, name, outfile):
+def getResponsiveMaintainersScore(username, token, owner, name):
     #username = "lizziesarah"
     #token = "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"
     # header = {'Authorization': f'Bearer {token}'}
@@ -115,16 +108,18 @@ def getResponsiveMaintainersScore(username, token, owner, name, outfile):
     rm_score = 0
     if numCommits > 1:
         rm_score = 1
-    outfile_pointer = open(outfile, "a")
-    outfile_pointer.write(f'\n\t'+'{'+f'\n\t\tResponsive Maintainers: {rm_score}\n'+'\t}')
-    outfile_pointer.close()
+
     #print(f"Responsive Maintainers: {rm_score}")
     return(rm_score)
 
 def finalScore(bf, lc, cr, ru, rm, outfile):
-    score = (bf*4+lc*4+cr*3+ru*2+rm*1) /14
+    score = (bf*4+lc*4+cr*3+ru*2+rm*1) / 14
+    score_truncated = round(score, 3)
     outfile_pointer = open(outfile, "a")
-    outfile_pointer.write(f'\n\t'+'{'+f'\t\tTotal Score: {score}\n'+'\t}'+'\n]')
+    outfile_pointer.write(f'\n\t' + '{' + f'\n\t\tCorrectness: {cr}\n' + '\t}')
+    outfile_pointer.write(f'\n\t' + '{' + f'\n\t\tResponsive Maintainers: {rm}\n' + '\t}')
+    outfile_pointer.write(f'\n\t'+'{'+f'\n\t\tBus Factor: {bf}\n'+'\t}')
+    outfile_pointer.write(f'\n\t'+'{'+f'\n\t\tTotal Score: {score_truncated}\n'+'\t}'+'\n]')
     outfile_pointer.close()
     return score
 
@@ -133,9 +128,9 @@ if __name__ == '__main__':
     owners_and_names = {}
     owners_and_names = readFile(urlfile='urlfile_actual.txt')
     for owner in owners_and_names:
-        getCorrectnessScore(username='lizziesarah', token='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', owner=owner, name=owners_and_names[owner], outfile='outfile.txt')
-        getResponsiveMaintainersScore(username='lizziesarah', token='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', owner=owner, name=owners_and_names[owner], outfile='outfile.txt')
-        getBusFactorScore(username='lizziesarah', token='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', owner=owner, name=owners_and_names[owner], outfile='outfile.txt')
-
+        cr = getCorrectnessScore(username='lizziesarah', token='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', owner=owner, name=owners_and_names[owner])
+        rm = getResponsiveMaintainersScore(username='lizziesarah', token='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', owner=owner, name=owners_and_names[owner])
+        bf = getBusFactorScore(username='lizziesarah', token='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', owner=owner, name=owners_and_names[owner])
+        finalScore(bf=bf, cr=cr, rm=rm, lc=0, ru=0, outfile='outfile.txt')
 
 
