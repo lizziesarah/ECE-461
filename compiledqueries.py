7fd67f530de57060202dd6e06478e0f522773a7e
lizziesarah <102872411+lizziesarah@users.noreply.github.com>
2023-02-10 13:19:21 -0800

    can read github url from .txt file

--- compiledqueries.py
+++ compiledqueries.py
@@ -1,13 +1,36 @@
 import requests
 from datetime import date
 
-def getBusFactorScore(username, token, owner, name):
+# https://github.com/octocat/Hello-World
+
+def readFile(urlfile):
+    owners_and_names = {}
+    file_pointer = open(urlfile, "r")
+
+    for line in file_pointer.readlines():
+        owner = ""
+        name = ""
+        # its a github file
+        i=19
+        if line[8] == 'g':
+            while line[i] != '/':
+                owner += line[i]
+                i+=1
+            name = line[i+1:]
+
+
+
+        owners_and_names[owner] = name.strip('\n')
+        return owners_and_names
+
+
+def getBusFactorScore(username, token, owner, name, outfile):
     #username = "lizziesarah"
     #token = "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"
     header = {'Authorization': 'Bearer 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'}
 
-    owner = '"' + "octocat" + '"'
-    name = '"' + "Hello-World" + '"'
+    owner = '"' + f"{owner}" + '"'
+    name = '"' + f"{name}" + '"'
 
     query1 = "{\n" + f"\trepository(owner: {owner}, name: {name})\n" + "\t{ mentionableUsers{\n\ttotalCount\n}\n}\n}"
 
@@ -22,15 +45,18 @@ def getBusFactorScore(username, token, owner, name):
         score = numContributors / 5
     #print(f"Number of contributors: {numContributors}")
     #print(f"Bus Factor: {score}")
+    outfile_pointer = open(outfile, "a")
+    outfile_pointer.write(f'\n\t'+'{'+f'\n\t\tBus Factor: {score}\n'+'\t}')
+    outfile_pointer.close()
     return score
 
-def getCorrectnessScore(username, token, owner, name) :
+def getCorrectnessScore(username, token, owner, name, outfile) :
     #username = "lizziesarah"
     #token = "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"
     header = {'Authorization': 'Bearer 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'}
 
-    #owner = '"' + "octocat" + '"'
-    #name = '"' + "Hello-World" + '"'
+    owner = '"' + f"{owner}" + '"'
+    name = '"' + f"{name}" + '"'
 
     query1 = "{\n" + f"\trepository(owner: {owner}, name: {name})" + " { \n\t\tstargazerCount }}"
 
@@ -39,13 +65,21 @@ def getCorrectnessScore(username, token, owner, name) :
     result = req.json()
 
     number_of_stars = result['data']['repository']['stargazerCount']
-    print(f"Number of Stars: {number_of_stars}")
+
+    score = 0
     if number_of_stars > 5:
+        score=1
+        outfile_pointer = open(outfile, "a")
+        outfile_pointer.write(f'\n\t'+'{'+f'\n\t\tCorrectness: {score}\n'+'\t}')
+        outfile_pointer.close()
         return 1
     else:
+        outfile_pointer = open(outfile, "a")
+        outfile_pointer.write(f'\n\t'+'{'+f'\n\t\tCorrectness: {score}\n'+'\t}')
+        outfile_pointer.close()
         return 0
 
-def getResponsiveMaintainersScore(username, token, owner, name):
+def getResponsiveMaintainersScore(username, token, owner, name, outfile):
     #username = "lizziesarah"
     #token = "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"
     # header = {'Authorization': f'Bearer {token}'}
@@ -62,21 +96,46 @@ def getResponsiveMaintainersScore(username, token, owner, name):
     if todaysDateDateTime.day < 10 and todaysDateDateTime.month < 10:
         gts = '"' + str(lastyear) + "-0" + str(todaysDateDateTime.month) + "-0" + str(
             todaysDateDateTime.day) + "T01:01:00Z" + '"'
-    if todaysDateDateTime.day < 10 and todaysDateDateTime.month > 10:
+    if todaysDateDateTime.day < 10 and todaysDateDateTime.month >= 10:
         gts = '"' + str(lastyear) + "-" + str(todaysDateDateTime.month) + "-0" + str(
             todaysDateDateTime.day) + "T01:01:00Z" + '"'
-    if todaysDateDateTime.day > 10 and todaysDateDateTime.month < 10:
+    if todaysDateDateTime.day >= 10 and todaysDateDateTime.month < 10:
         gts = '"' + str(lastyear) + "-0" + str(todaysDateDateTime.month) + "-" + str(
+            todaysDateDateTime.day) + "T01:01:00Z" + '"'
+    if todaysDateDateTime.day >= 10 and todaysDateDateTime.month >= 10:
+        gts = '"' + str(lastyear) + "-" + str(todaysDateDateTime.month) + "-" + str(
             todaysDateDateTime.day) + "T01:01:00Z" + '"'
 
     query1 = "{\n" + f"\trepository(owner: {owner}, name: {name})" + " { \n" + "\t ref(qualifiedName:" + f" {master})" + " { " + "\n\t\ttarget { \n\t\t ... on Commit {\n\t" + f"history(since:{gts})" + "{" + "\n\t\ttotalCount}}}}}\n\t\t}"
 
     req = requests.post(url='https://api.github.com/graphql', json={'query': query1}, auth=(username, token))
     result = req.json()
+
     numCommits = result['data']['repository']['ref']['target']['history']['totalCount']
     rm_score = 0
     if numCommits > 1:
         rm_score = 1
-    # print(f"Number of commits since {todaysDateDateTime}: {numCommits}")
+    outfile_pointer = open(outfile, "a")
+    outfile_pointer.write(f'\n\t'+'{'+f'\n\t\tResponsive Maintainers: {rm_score}\n'+'\t}')
+    outfile_pointer.close()
     #print(f"Responsive Maintainers: {rm_score}")
     return(rm_score)
+
+def finalScore(bf, lc, cr, ru, rm, outfile):
+    score = (bf*4+lc*4+cr*3+ru*2+rm*1) /14
+    outfile_pointer = open(outfile, "a")
+    outfile_pointer.write(f'\n\t'+'{'+f'\t\tTotal Score: {score}\n'+'\t}'+'\n]')
+    outfile_pointer.close()
+    return score
+
+
+if __name__ == '__main__':
+    owners_and_names = {}
+    owners_and_names = readFile(urlfile='urlfile_actual.txt')
+    for owner in owners_and_names:
+        getCorrectnessScore(username='lizziesarah', token='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', owner=owner, name=owners_and_names[owner], outfile='outfile.txt')
+        getResponsiveMaintainersScore(username='lizziesarah', token='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', owner=owner, name=owners_and_names[owner], outfile='outfile.txt')
+        getBusFactorScore(username='lizziesarah', token='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', owner=owner, name=owners_and_names[owner], outfile='outfile.txt')
+
+
+
