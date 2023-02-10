dc5805441d79e2cfcf1cae0340869b3234f4c998
Collin Ronald Sell <crsell@eceprog1.ecn.purdue.edu>
2023-02-10 17:08:32 -0500

    final

--- cloning repos/github_query_clone.py
+++ cloning repos/github_query_clone.py
@@ -1,35 +1,21 @@
-from github import Github
 import git
 import os
-from dotenv import load_dotenv
 from os.path import exists
-load_dotenv()
-
-g = Github(os.getenv('GITHUB_TOKEN'))
 
 def clone_repo():
     n = 0
-    with open(r"url_file.txt", "r") as url_file:
+    with open(r"git_urls.txt", "r") as url_file:
         urls = url_file.readlines()
     for url in urls:
         url = url.strip()
         n += 1
         new_dir_name = "cloned_repo{}".format(n)
         os.mkdir(new_dir_name)
-        url_split = url.split('/')
-        print(url_split)
-        if url_split[2] == 'github.com':
-            repo = git.Repo.clone_from(url, new_dir_name)
-            readme_exists = exists("{}/README.md".format(new_dir_name))
+        repo = git.Repo.clone_from(url, new_dir_name)
+        readme_exists = exists("{}/README.md".format(new_dir_name))
+        if readme_exists:
+            rampup_time = 1
         else:
-            registry_name = url_split[4]
-            query = registry_name + '+in:readme+in:description'
-            repos = g.search_repositories(query, 'stars', 'desc')
-            for repo in repos:
-                split = repo.clone_url.split('/')
-                print(repo.clone_url)
-                if split[4] == registry_name:
-                    print(repo.clone_url)
-                    break
+            rampup_time = 0
             
 clone_repo()
